93b3b07d418a7cee01f8a543709dbb6ff96c888d
Nicholas Martin <nicholas.martin89@gmail.com>
2018-10-19 10:53:21 -0700

    Set up working environment, outlined server and webpack

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
